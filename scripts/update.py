7452b37d1d711699be08b6644c36447cb4c5c292
JackPiri <gpirinoli@horizenlabs.io>
2024-07-01 09:33:08 +0200

    init

---
+++ scripts/update.py
@@ -0,0 +1,12 @@
+"""
+A script to update dependencies
+"""
+
+import argparse
+
+def a_function(an_argument):
+    return 0
+
+if __name__ == "__main__":
+    args = a_function(0)
+    print(f"returned: {args}")
